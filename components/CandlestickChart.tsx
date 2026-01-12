18679a4a8af1f90f7e18041784a50c63824b74bc
Adi Jain <adi1510jain@gmail.com>
2026-01-12 19:01:02 +0530

    Implemented CandleStick-Chart

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -71,7 +71,17 @@ const CandlestickChart = ({
             CandlestickSeries,
             getCandlestickConfig()
         );
-        series.setData(convertOHLCData(ohlcData));
+        const convertedToSeconds = ohlcData.map(
+            (item) =>
+                [
+                    Math.floor(item[0] / 1000),
+                    item[1],
+                    item[2],
+                    item[3],
+                    item[4],
+                ] as OHLCData
+        );
+        series.setData(convertOHLCData(convertedToSeconds));
         chart.timeScale().fitContent();
 
         chartRef.current = chart;
